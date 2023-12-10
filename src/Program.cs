3ae0615352bbd46f4a9b06a17e14c9c61b02e8c8
Gordon Beeming <gpg@gordonbeeming.com>
2023-12-11 01:55:18 +1000

    Add calculations for Square, Pentagon, and Hexagon shapes

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,13 @@
 ﻿Console.WriteLine("Area Circle (2.5): {0}", ShapeCalculator.CalculateArea(Shape.Circle, 2.5));
 Console.WriteLine("Area Rectangle (2.5, 3.5): {0}", ShapeCalculator.CalculateArea(Shape.Rectangle, 2.5, 3.5));
 Console.WriteLine("Area Triangle (2.5, 3.5, 4.5): {0}", ShapeCalculator.CalculateArea(Shape.Triangle, 2.5, 3.5, 4.5));
+Console.WriteLine("Area Square (2.5): {0}", ShapeCalculator.CalculateArea(Shape.Square, 2.5));
+Console.WriteLine("Area Pentagon (2.5): {0}", ShapeCalculator.CalculateArea(Shape.Pentagon, 2.5));
+Console.WriteLine("Area Hexagon (2.5): {0}", ShapeCalculator.CalculateArea(Shape.Hexagon, 2.5));
 
 Console.WriteLine("Perimeter Circle (2.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Circle, 2.5));
 Console.WriteLine("Perimeter Rectangle (2.5, 3.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Rectangle, 2.5, 3.5));
 Console.WriteLine("Perimeter Triangle (2.5, 3.5, 4.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Triangle, 2.5, 3.5, 4.5));
+Console.WriteLine("Perimeter Square (2.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Square, 2.5));
+Console.WriteLine("Perimeter Pentagon (2.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Pentagon, 2.5));
+Console.WriteLine("Perimeter Hexagon (2.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Hexagon, 2.5));
