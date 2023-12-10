09c07a89099f4cc30c03293d7b96e67dc15ffc15
Gordon Beeming <gpg@gordonbeeming.com>
2023-12-11 01:54:00 +1000

    Added debugging

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Area Circle (2.5): {0}", ShapeCalculator.CalculateArea(Shape.Circle, 2.5));
+Console.WriteLine("Area Rectangle (2.5, 3.5): {0}", ShapeCalculator.CalculateArea(Shape.Rectangle, 2.5, 3.5));
+Console.WriteLine("Area Triangle (2.5, 3.5, 4.5): {0}", ShapeCalculator.CalculateArea(Shape.Triangle, 2.5, 3.5, 4.5));
+
+Console.WriteLine("Perimeter Circle (2.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Circle, 2.5));
+Console.WriteLine("Perimeter Rectangle (2.5, 3.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Rectangle, 2.5, 3.5));
+Console.WriteLine("Perimeter Triangle (2.5, 3.5, 4.5): {0}", ShapeCalculator.CalculatePerimeter(Shape.Triangle, 2.5, 3.5, 4.5));
